a523b13acf9ea6b00dae8a320fd7b40eb8dc0d02
husnulhotimah107 <husnulhotimah9c@gmail.com>
2020-11-02 23:20:12 +0700

    UTS JARINGAN

---
+++ src/Socket/Data.java
@@ -0,0 +1,50 @@
+package Socket;
+
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+
+public class Data implements Serializable {
+    
+    private String status;
+    private ImageIcon gambar;
+    private byte[] file;
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ImageIcon getImage() {
+        return gambar;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.gambar = image;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+}
